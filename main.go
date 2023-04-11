20de2c7cdd8c9020cf6ce04777cc6a69de66d759
Vojtech Kronika <nesty.wildstar@gmail.com>
2023-04-11 10:48:28 +0200

    Added option to enter file to process

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from my finance tool!")
+	fmt.Print("Enter the name of the file you want to load:")
+
+	var fileName string
+	fmt.Scanf("%s", &fileName)
+
+	file, err := os.Open(fileName) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(file)
 }
