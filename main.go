8051af084ac089e34eed8db641ef6f322b029898
Vojtech Kronika <nesty.wildstar@gmail.com>
2023-05-31 14:08:39 +0200

    Refactoring the code a bit

--- main.go
+++ main.go
@@ -10,6 +10,49 @@ import (
 	"syscall"
 )
 
+const (
+	airbank = "a"
+	trezor  = "t"
+	moneta  = "m"
+	degiro  = "d"
+	sp      = "c"
+	trading = "212"
+	merge   = "m"
+	load    = "l"
+)
+
+var (
+	spaces = 12
+)
+
+func main() {
+	// Register a signal handler for the SIGHUP signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP)
+	go handleSignal(sigs)
+
+	options := []string{"[m] merge"}
+	for {
+		prompt := "Choose from [l] load\n"
+		for _, option := range options {
+			prompt += fmt.Sprintf("%*s%s\n", spaces, "", option)
+		}
+		prompt += "Enter your choice: "
+		fmt.Printf(prompt)
+		var choice string
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case load:
+			loadUser()
+		case merge:
+			mergePortfolios()
+		default:
+			fmt.Println("Invalid choice")
+		}
+	}
+}
+
 // SIGHUP signal handler
 func handleSignal(sigs chan os.Signal) {
 	<-sigs
@@ -17,33 +60,159 @@ func handleSignal(sigs chan os.Signal) {
 	os.Exit(0)
 }
 
-// Prompt user for input
-func promptUser() string {
-	options := []string{"[m] merge", "[r] remove"}
-	spaces := 12
-	prompt := "Choose from [l] load\n"
-	for _, option := range options {
-		prompt += fmt.Sprintf("%*s%s\n", spaces, "", option)
-	}
-	prompt += "Enter your choice: "
-	fmt.Print(prompt)
-	var choice string
-	fmt.Scanln(&choice)
-	return choice
-}
-
-func promptLoadUser() string {
+func loadUser() {
 	options := []string{"[t] trezor", "[m] moneta", "[d] degiro", "[c] ceska sporitelna", "[212] trading212"}
-	spaces := 12
 	prompt := "Choose from [a] airbank\n"
 	for _, option := range options {
 		prompt += fmt.Sprintf("%*s%s\n", spaces, "", option)
 	}
 	prompt += "Enter your choice: "
-	fmt.Print(prompt)
+	fmt.Printf(prompt)
 	var choice string
 	fmt.Scanln(&choice)
-	return choice
+
+	switch choice {
+	case airbank:
+		loadAirBank()
+	case trezor:
+		loadTrezor()
+	case moneta:
+		loadMoneta()
+	case degiro:
+		loadDegiro()
+	case sp:
+		loadCeskaSporitelna()
+	case trading:
+		loadTrading212()
+	default:
+		fmt.Println("Invalid choice")
+	}
+}
+
+func loadAirBank() {
+	fmt.Print("Enter the path to the directory of files: ")
+	var dirPath string
+	fmt.Scanln(&dirPath)
+
+	statement, err := mergeAirBankStatements(dirPath)
+	if err != nil {
+		log.Printf("Error merging AirBank statements: %v", err)
+		return
+	}
+
+	fmt.Println(statement.AccountNumber)
+	fmt.Println(statement.StartDate)
+	fmt.Println(statement.EndDate)
+	fmt.Println(len(statement.Transactions))
+
+	*statement = sortTransactions(*statement)
+	value := sumTransactions(*statement)
+
+	fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value, statement.Currnecy)
+	saveSoaJson(*statement)
+}
+
+func loadTrezor() {
+	fmt.Print("Enter the path to the directory of files: ")
+	var dirPath string
+	fmt.Scanln(&dirPath)
+
+	_, err := convertTrezorToStatement(dirPath)
+	if err != nil {
+		log.Printf("Error converting Trezor files: %v", err)
+		return
+	}
+
+	fmt.Println("Conversion successful")
+}
+
+func loadMoneta() {
+	fmt.Print("Enter the path to the file: ")
+	var filePath string
+	fmt.Scanln(&filePath)
+
+	xmlData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		return
+	}
+
+	statement, err := parseMonetaStatement(xmlData)
+	if err != nil {
+		log.Printf("Error parsing Moneta statement: %v", err)
+		return
+	}
+	saveSoaJson(statement)
+
+	value := sumTransactions(statement)
+	fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value, statement.Currnecy)
+}
+
+func loadDegiro() {
+	fmt.Print("Enter the path to the file: ")
+	var filePath string
+	fmt.Scanln(&filePath)
+
+	csvData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		return
+	}
+
+	portfolio, err := parseDegiroPortfolio(csvData, "degiro")
+	savePortfolioJson(portfolio)
+	if err != nil {
+		log.Printf("Error parsing Degiro portfolio: %v", err)
+		return
+	}
+
+	value := portfolioValue(portfolio)
+	fmt.Printf("Value of your Degiro portfolio is %.2f %s\n", value, "EUR")
+}
+
+func loadCeskaSporitelna() {
+	fmt.Print("Enter the path to the file: ")
+	var filePath string
+	fmt.Scanln(&filePath)
+
+	jsonData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		return
+	}
+
+	statement, err := parseCeskaSporitelnaStatement(jsonData)
+	if err != nil {
+		log.Printf("Error parsing Ceska Sporitelna statement: %v", err)
+		return
+	}
+	saveSoaJson(statement)
+
+	value := sumTransactions(statement)
+	fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value, statement.Currnecy)
+}
+
+func loadTrading212() {
+	fmt.Print("Enter the path to the file: ")
+	var filePath string
+	fmt.Scanln(&filePath)
+
+	csvData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		return
+	}
+
+	txs, err := parseTrading212History(csvData)
+	if err != nil {
+		log.Printf("Error parsing Trading 212 history: %v", err)
+		return
+	}
+	portfolio := TransactionsToPortfolio(txs, "trading212")
+	savePortfolioJson(portfolio)
+
+	value := portfolioValue(portfolio)
+	fmt.Printf("Value of your Trading 212 portfolio is %.2f %s\n", value, "EUR")
 }
 
 // Parse AirBank statement files and merge them
@@ -121,150 +290,36 @@ func convertTrezorToStatement(dirPath string) (*StatementOfAccount, error) {
 		fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value*btcCZK, "CZK")
 		saveSoaJson(*statement)
 	}
-	fmt.Scanln()
 
 	return nil, nil
 }
 
-func main() {
-	// Register a signal handler for the SIGHUP signal
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGHUP)
-	go handleSignal(sigs)
-	for {
-		choice1 := promptUser()
-		switch choice1 {
-		case "l":
-			choice2 := promptLoadUser()
-			switch choice2 {
-			case "a":
-				fmt.Print("You chose AirBank. Enter the path to the directory of files: ")
-				var dirPath string
-				fmt.Scanf("%s", &dirPath)
-				statement, err := mergeAirBankStatements(dirPath)
-				if err != nil {
-					log.Printf("Error merging statements: %v", err)
-					continue
-				}
-				fmt.Println(statement.AccountNumber)
-				fmt.Println(statement.StartDate)
-				fmt.Println(statement.EndDate)
-				fmt.Println(len(statement.Transactions))
-				*statement = sortTransactions(*statement)
-				value := sumTransactions(*statement)
-				fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value, statement.Currnecy)
-				saveSoaJson(*statement)
-				fmt.Scanln()
-			case "t":
-				fmt.Print("You chose Trezor. Enter the path to the directory of files: ")
-				var dirPath string
-				fmt.Scanf("%s", &dirPath)
-				_, err := convertTrezorToStatement(dirPath)
-				if err != nil {
-					log.Printf("Error converting Trezor files: %v", err)
-					continue
-				}
-				fmt.Scanln()
-			case "m":
-				fmt.Print("You chose Moneta. Enter the path to the file: ")
-				var filePath string
-				fmt.Scanf("%s", &filePath)
-				xmlData, err := ioutil.ReadFile(filePath)
-				if err != nil {
-					log.Printf("Error reading file: %v", err)
-					continue
-				}
-				statement, err := parseMonetaStatement(xmlData)
-				if err != nil {
-					log.Printf("Error parsing Moneta statement: %v", err)
-					continue
-				}
-				saveSoaJson(statement)
-				value := sumTransactions(statement)
-				fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value, statement.Currnecy)
-				fmt.Scanln()
-			case "d":
-				fmt.Print("You chose Degiro. Enter the path to the file: ")
-				var filePath string
-				fmt.Scanf("%s", &filePath)
-				csvData, err := ioutil.ReadFile(filePath)
-				if err != nil {
-					log.Printf("Error reading file: %v", err)
-					continue
-				}
-				portfolio, err := parseDegiroPortfolio(csvData, "degiro")
-				savePortfolioJson(portfolio)
-				if err != nil {
-					log.Printf("Error parsing Degiro portfolio: %v", err)
-					continue
-				}
-				value := portfolioValue(portfolio)
-				fmt.Printf("Value of your degiro portfolio is %.2f %s\n", value, "EUR")
-				fmt.Scanln()
-			case "c":
-				fmt.Print("You chose Ceska Sporitelna. Enter the path to the file: ")
-				var filePath string
-				fmt.Scanf("%s", &filePath)
-				jsonData, err := ioutil.ReadFile(filePath)
-				if err != nil {
-					log.Printf("Error reading file: %v", err)
-					continue
-				}
-				statement, err := parseCeskaSporitelnaStatement(jsonData)
-				if err != nil {
-					log.Printf("Error parsing Ceska Sporitelna statement: %v", err)
-					continue
-				}
-				saveSoaJson(statement)
-				value := sumTransactions(statement)
-				fmt.Printf("Value of account %s is %.2f %s\n", statement.AccountNumber, value, statement.Currnecy)
-				fmt.Scanln()
-			case "212":
-				fmt.Print("You chose Trading 212. Enter the path to the file: ")
-				var filePath string
-				fmt.Scanf("%s", &filePath)
-				csvData, err := ioutil.ReadFile(filePath)
-				if err != nil {
-					log.Printf("Error reading file: %v", err)
-					continue
-				}
-				txs, err := parseTrading212History(csvData)
-				if err != nil {
-					log.Printf("Error parsing trading 212 history: %v", err)
-					continue
-				}
-				portfolio := TransactionsToPortfolio(txs, "trading212")
-				savePortfolioJson(portfolio)
-				value := portfolioValue(portfolio)
-				fmt.Printf("Value of your trading212 portfolio is %.2f %s\n", value, "EUR")
-				fmt.Scanln()
-			default:
-				fmt.Println("Invalid choice")
-			}
-		case "m":
-			fmt.Print("You chose merge portfolios. Enter the first file: ")
-			var filePath string
-			fmt.Scanf("%s", &filePath)
-			source, err := loadPortfolioJson(filePath)
-			if err != nil {
-				log.Printf("Error loading portfolio: %v", err)
-				continue
-			}
-			fmt.Scanln()
-			fmt.Print("Enter the second file: ")
-			fmt.Scanf("%s", &filePath)
-			destination, err := loadPortfolioJson(filePath)
-			if err != nil {
-				log.Printf("Error loading portfolio: %v", err)
-				continue
-			}
-			merged := MergePortfolios(*source, *destination)
-			savePortfolioJson(merged)
-			value := portfolioValue(merged)
-			fmt.Printf("Value of your merged portfolio is %.2f %s\n", value, "EUR")
-			fmt.Scanln()
-		default:
-			fmt.Println("Invalid choice")
-		}
+func mergePortfolios() {
+	fmt.Print("Enter the first file: ")
+	var filePath1 string
+	fmt.Scanln(&filePath1)
+
+	source, err := loadPortfolioJson(filePath1)
+	if err != nil {
+		log.Printf("Error loading portfolio: %v", err)
+		return
 	}
+	fmt.Println("File loaded")
+
+	fmt.Print("Enter the second file: ")
+	var filePath2 string
+	fmt.Scanln(&filePath2)
+
+	destination, err := loadPortfolioJson(filePath2)
+	if err != nil {
+		log.Printf("Error loading portfolio: %v", err)
+		return
+	}
+	fmt.Println("File loaded")
+
+	merged := MergePortfolios(*source, *destination)
+	savePortfolioJson(merged)
+
+	value := portfolioValue(merged)
+	fmt.Printf("Value of your merged portfolio is %.2f %s\n", value, "EUR")
 }
